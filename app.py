cb23f465afbe9b3c4cda847de3ca940a56a185a6
ikhwanashshiddiqi <12250311668@students.uin-suska.ac.id>
2026-01-03 12:37:24 +0700

    Add initial project files: .gitignore, Procfile, app.py, and requirements.txt

--- app.py
+++ app.py
@@ -184,4 +184,5 @@ def index():
 if __name__ == '__main__':
     os.makedirs('static/uploads', exist_ok=True)
     os.makedirs('static/results', exist_ok=True)
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
